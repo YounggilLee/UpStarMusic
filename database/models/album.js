5c415dc5d94841a4cc84ed203d08e1d0e0f3cded
GiL <iamleeyounggil@gmail.com>
2018-04-18 09:54:58 -0400

    Feat: Build artist and album schema

--- database/models/album.js
+++ database/models/album.js
@@ -1,1 +1,15 @@
 // Todo: create Album Schema
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const AlbumSchema = new Schema({
+    title: String,
+    date: Date,
+    copiesSolid: Number,
+    nomberTracks: Number,
+    image: String,
+    revenue: Number
+})
+
+module.exports = AlbumSchema
