b8a816fa873987b509f6c4271c686fc69c154be7
GiL <iamleeyounggil@gmail.com>
2018-04-19 10:28:35 -0400

    Implement searching user data by using an age range selector

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+    const minQuery = Artist
+        .find({})                      //return array
+        .sort({ age: 1 })               //acending sort
+        .limit(1)                      // get first on
+        .then(artists => artists[0].age) //still array
+
+    const maxQuery = Artist
+        .find({})                      //return array
+        .sort({ age: -1 })               //decending sort
+        .limit(1)                      // get first on
+        .then(artists => artists[0].age) //still array
+
+    return Promise.all([minQuery, maxQuery])
+        .then((result) => {
+            return { min: result[0], max: result[1] }
+        })
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,20 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+    const minQuery = Artist
+     .find({})                      //return array
+     .sort({ yearsActive: 1 })               //acending sort
+     .limit(1)                      // get first on
+        .then(artists => artists[0].yearsActive) //still array
+
+    const maxQuery = Artist
+        .find({})                      //return array
+        .sort({ yearsActive: -1 })               //decending sort
+        .limit(1)                      // get first on
+        .then(artists => artists[0].yearsActive) //still array
+
+    return Promise.all([minQuery, maxQuery])
+        .then((result) => {
+            return{ min: result[0], max: result[1] }
+        })
 };
